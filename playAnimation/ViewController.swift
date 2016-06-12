a3be5e8c265d55d0805f4fe7708e8f29a526d850
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-06-12 08:47:45 +0800

    Initial Commit

---
+++ playAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  playAnimation
+//
+//  Created by MikeWu on 2016/6/12.
+//  Copyright © 2016年 MSMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
