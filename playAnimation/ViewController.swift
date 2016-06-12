9ddc44219a8d990e735a235c678605d795bad28e
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-06-12 09:02:26 +0800

    a simple case to show how to make a object in iOS App to do below action. 1) translate 2) rotate 3) scale 4) change background color 5) change alpha 6) reset

--- playAnimation/ViewController.swift
+++ playAnimation/ViewController.swift
@@ -1,15 +1,10 @@
-//
-//  ViewController.swift
-//  playAnimation
-//
-//  Created by MikeWu on 2016/6/12.
-//  Copyright © 2016年 MSMedia. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var block: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +14,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func translateBtn(sender: AnyObject) {
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            self.block.transform = CGAffineTransformMakeTranslation(100, 100)
+            }, completion: nil)
+    }
 
+    @IBAction func rotateBtn(sender: AnyObject) {
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            self.block.transform = CGAffineTransformMakeRotation(CGFloat(M_PI / 4.0))
+            }, completion: nil)
+    }
 
+    @IBAction func colorBtn(sender: AnyObject) {
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            self.block.backgroundColor = UIColor.blueColor()
+            }, completion: nil)
+    }
+    
+    @IBAction func scaleBtn(sender: AnyObject) {
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            self.block.transform = CGAffineTransformMakeScale(2, 2)
+            }, completion: nil)
+    }
+    
+    @IBAction func alphaBtn(sender: AnyObject) {
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            self.block.alpha = 0.1
+            }, completion: nil)
+    }
+    
+    @IBAction func resetBtn(sender: AnyObject) {
+        self.block.transform = CGAffineTransformConcat(CGAffineTransformMakeTranslation(0, 0), CGAffineTransformMakeScale(1, 1))
+        self.block.backgroundColor = UIColor.redColor()
+        self.block.alpha = 1
+    }
 }
 
